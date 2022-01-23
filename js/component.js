defec8e84522fae9baa978e001c200660f1c31dc
amit thakur <amithakur081@gmail.com>
2022-01-23 14:19:11 +0530

    Fix : toggle sidebar

--- js/component.js
+++ js/component.js
@@ -1,10 +1,21 @@
 const menu = document.querySelector(".menu");
+const sidebar = document.querySelector(".sidebar");
+const contentBox = document.querySelector(".content-box");
+const content = document.querySelector(".content")
+const sidebarItem = document.querySelectorAll(".sidebar-item")
+const header = document.querySelector("nav")
+
 function toggle() {
-  const sidebar = document.querySelector(".sidebar");
-  const content = document.querySelector(".content-box");
-
   sidebar.classList.toggle("active");
-  content.classList.toggle("active");
+  contentBox.classList.toggle("active");
 }
-
 menu.addEventListener("click", toggle);
+
+sidebarItem.forEach(function(link){
+
+  link.addEventListener('click',function(e){
+    sidebar.classList.remove("active");
+    contentBox.classList.remove("active");
+
+  })
+})
